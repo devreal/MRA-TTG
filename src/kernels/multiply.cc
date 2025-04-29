ff593f88aa06a4331469c6bafdcbc120066a9259
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2025-04-29 14:35:37 -0400

    Fix explicit instantiation of multiply kernel
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- src/kernels/multiply.cc
+++ src/kernels/multiply.cc
@@ -7,15 +7,18 @@ namespace mra {
   template
   void submit_multiply_kernel<double, 3>(
     const Domain<3>& D,
+    const Key<3>& keyA,
+    const Key<3>& keyB,
     const TensorView<double, 3+1>& funcA,
     const TensorView<double, 3+1>& funcB,
     TensorView<double, 3+1>& funcR,
+    const TensorView<double, 2>& hgT,
+    const TensorView<double, 2>& phi,
     const TensorView<double, 2>& phiT,
     const TensorView<double, 2>& phibar,
+    const TensorView<double, 1>& quad_x,
     size_type N,
     size_type K,
-    const Key<3>& key,
     double* tmp,
     ttg::device::Stream stream);
-
 } // namespace mra
