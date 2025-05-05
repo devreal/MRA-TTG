f424f092b98a42a22aa2e5515278eb8b853a615f
nilesh <n.chaturvedi3@gmail.com>
2025-05-05 18:54:47 -0400

    class init

---
+++ include/mra/misc/convolutiondata.h
@@ -0,0 +1,19 @@
+#ifndef MRA_CONVOLUTIONDATA_H
+#define MRA_CONVOLUTIONDATA_H
+
+#include "mra/misc/types.h"
+#include "mra/misc/hash.h"
+#include "mra/misc/misc.h"
+#include "mra/misc/platform.h"
+
+
+namespace mra {
+
+  template <typename T, Dimension NDIM>
+  class ConvolutionData {
+
+  };
+
+} // namespace mra
+
+#endif // MRA_CONVOLUTIONDATA_H
