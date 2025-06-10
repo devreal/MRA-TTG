c00eab02a934d4fdb2ba6008ea9b60233ea916ab
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2025-06-10 19:38:32 +0200

    Derivative: use project-down nodes instead of inputs nodes
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- include/mra/kernels/derivative.h
+++ include/mra/kernels/derivative.h
@@ -127,11 +127,11 @@ namespace mra {
       parent_to_child(D, right,  key.neighbor(axis, 1), node_right, right_tmp, tmp_result, phibar, phi, quad_x, K, workspace);
       deriv = 0;
 
-      transform_dir(node_left, operators((int)FunctionData<T, NDIM>::DerivOp::RPT), left_tmp, deriv, axis);
-      transform_dir(node_center, operators((int)FunctionData<T, NDIM>::DerivOp::R0T), center_tmp, deriv, axis);
-      transform_dir(node_right, operators((int)FunctionData<T, NDIM>::DerivOp::RMT), right_tmp, deriv, axis);
+      transform_dir(left_tmp, operators((int)FunctionData<T, NDIM>::DerivOp::RPT), tmp_result, deriv, axis);
+      transform_dir(center_tmp, operators((int)FunctionData<T, NDIM>::DerivOp::R0T), tmp_result, deriv, axis);
+      transform_dir(right_tmp, operators((int)FunctionData<T, NDIM>::DerivOp::RMT), tmp_result, deriv, axis);
 
-      T scale = std::sqrt(D.template get_reciprocal_width<T>(axis)*std::pow(T(2), T(key.level())));
+      T scale = D.template get_reciprocal_width<T>(axis)*std::pow(T(2), T(key.level()));
       T thresh = T(1e-12);
       deriv *= scale;
       deriv.reduce_rank(thresh);
