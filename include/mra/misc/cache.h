2b05939245af32b7cd938cf440c95999b0e721b7
nilesh <n.chaturvedi3@gmail.com>
2025-06-23 16:47:50 -0400

    Change template typename to represent generic value type

--- include/mra/misc/cache.h
+++ include/mra/misc/cache.h
@@ -5,11 +5,11 @@
 
 namespace mra{
 
-  template<typename TensorT, Dimension NDIM>
+  template<typename ValueT, Dimension NDIM>
   class Cache {
     private:
       typedef ConcurrentHashMap<Key<NDIM>> mapT;
-      typedef std::pair<Key<NDIM>, TensorT> pairT;
+      typedef std::pair<Key<NDIM>, ValueT> pairT;
       mapT cache;
 
     public:
@@ -25,7 +25,7 @@ namespace mra{
         return *this;
       }
 
-      inline const TensorT* getptr(const Key<NDIM>& key) const {
+      inline const ValueT* getptr(const Key<NDIM>& key) const {
         auto it = cache.find(key);
         if(it != cache.end()) {
           return &(it->second);
@@ -34,26 +34,26 @@ namespace mra{
         }
       }
 
-      inline const TensorT* getptr(Level n, Translation l) const {
+      inline const ValueT* getptr(Level n, Translation l) const {
         Key<NDIM> key(n, std::array<Translation, NDIM>(l));
         return getptr(key);
       }
 
-      inline const TensorT* getptr(Level n, const Key<NDIM>& disp) const {
+      inline const ValueT* getptr(Level n, const Key<NDIM>& disp) const {
         Key<NDIM> key(n, disp.translation);
         return getptr(key);
       }
 
-      inline void set(const Key<NDIM>& key, const TensorT& val) {
+      inline void set(const Key<NDIM>& key, const ValueT& val) {
         auto && [it, inserted] = cache.insert(pairT(key, val));
       }
 
-      inline void set(Level n, Translation l, const TensorT& val) {
+      inline void set(Level n, Translation l, const ValueT& val) {
         Key<NDIM> key(n, std::array<Translation, NDIM>(l));
         set(key, val);
       }
 
-      inline void set(Level n, const Key<NDIM>& disp, const TensorT& val) {
+      inline void set(Level n, const Key<NDIM>& disp, const ValueT& val) {
         Key<NDIM> key(n, disp.translation());
         set(key, val);
       }
