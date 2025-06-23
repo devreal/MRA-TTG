3775fb698608001ead9bab70fbd8d94aa4bcb354
nilesh <n.chaturvedi3@gmail.com>
2025-06-23 13:00:53 -0400

    Add Cache class to be a wrapper around hash map.

---
+++ include/mra/misc/cache.h
@@ -0,0 +1,63 @@
+#ifndef MRA_CACHE_H
+#define MRA_CACHE_H
+
+#include "mra/misc/types.h"
+
+namespace mra{
+
+  template<typename TensorT, Dimension NDIM>
+  class Cache {
+    private:
+      typedef ConcurrentHashMap<Key<NDIM>> mapT;
+      typedef std::pair<Key<NDIM>, TensorT> pairT;
+      mapT cache;
+
+    public:
+      Cache() : cache() {};
+
+      Cache(const Cache& c) : cache(c.cache) {};
+
+      Cache& operator=(const Cache& c) {
+        if(this != c) {
+          cache.clear();
+          cache = c.cache;
+        }
+        return *this;
+      }
+
+      inline const TensorT* getptr(const Key<NDIM>& key) const {
+        auto it = cache.find(key);
+        if(it != cache.end()) {
+          return &(it->second);
+        } else {
+          return nullptr;
+        }
+      }
+
+      inline const TensorT* getptr(Level n, Translation l) const {
+        Key<NDIM> key(n, std::array<Translation, NDIM>(l));
+        return getptr(key);
+      }
+
+      inline const TensorT* getptr(Level n, const Key<NDIM>& disp) const {
+        Key<NDIM> key(n, disp.translation);
+        return getptr(key);
+      }
+
+      inline void set(const Key<NDIM>& key, const TensorT& val) {
+        auto && [it, inserted] = cache.insert(pairT(key, val));
+      }
+
+      inline void set(Level n, Translation l, const TensorT& val) {
+        Key<NDIM> key(n, std::array<Translation, NDIM>(l));
+        set(key, val);
+      }
+
+      inline void set(Level n, const Key<NDIM>& disp, const TensorT& val) {
+        Key<NDIM> key(n, disp.translation());
+        set(key, val);
+      }
+  };
+}
+
+#endif // MRA_CACHE_H
