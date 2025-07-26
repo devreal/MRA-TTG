74f18f821474bc796a78770593aae9635cebd421
nilesh <n.chaturvedi3@gmail.com>
2025-07-26 17:35:36 -0400

    Add bounds check to `TensorView`

--- include/mra/tensor/tensorview.h
+++ include/mra/tensor/tensorview.h
@@ -427,6 +427,12 @@ namespace mra {
     template<typename... Dims>
     requires(!std::is_const_v<std::remove_reference_t<T>> && sizeof...(Dims) == NDIM && (std::is_integral_v<Dims>&&...))
     SCOPE value_type& operator()(Dims... idxs) {
+      std::array<size_type, sizeof...(Dims)> indices = {static_cast<size_type>(idxs)...};
+      for (size_type i = 0; i < indices.size(); ++i) {
+        if (indices[i] >= dim(i)) {
+          THROW("TensorView: index out of bounds");
+        }
+      }
       if (m_ptr == nullptr) THROW("TensorView: non-const call with nullptr");
       return m_ptr[offset(std::forward<Dims>(idxs)...)];
     }
@@ -436,6 +442,12 @@ namespace mra {
     requires(sizeof...(Dims) == NDIM && (std::is_integral_v<Dims>&&...))
     SCOPE const_value_type operator()(Dims... idxs) const {
       // let's hope the compiler will hoist this out of loops
+      std::array<size_type, sizeof...(Dims)> indices = {static_cast<size_type>(idxs)...};
+      for (size_type i = 0; i < indices.size(); ++i) {
+        if (indices[i] >= dim(i)) {
+          THROW("TensorView: index out of bounds");
+        }
+      }
       if (m_ptr == nullptr) {
         return T(0);
       } else {
@@ -449,6 +461,12 @@ namespace mra {
     template<typename... Dims>
     requires(sizeof...(Dims) < NDIM && (std::is_integral_v<Dims>&&...))
     SCOPE TensorView<T, NDIM-sizeof...(Dims)> operator()(Dims... idxs) const {
+      std::array<size_type, sizeof...(Dims)> indices = {static_cast<size_type>(idxs)...};
+      for (size_type i = 0; i < indices.size(); ++i) {
+        if (indices[i] >= dim(i)) {
+          THROW("TensorView: index out of bounds");
+        }
+      }
       constexpr const Dimension noffs = sizeof...(Dims);
       constexpr const Dimension ndim = NDIM-noffs;
       size_type offset = offset_impl<0>(std::forward<Dims>(idxs)...);
