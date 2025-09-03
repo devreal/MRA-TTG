0083bd4e733005e8de28a8aa36a1f77eb26912b1
nilesh <n.chaturvedi3@gmail.com>
2025-09-03 11:25:39 -0400

    Explicit template kernel instantiation

---
+++ src/kernels/convolution.cc
@@ -0,0 +1,21 @@
+
+#include "mra/kernels/convolution.h"
+
+
+namespace mra {
+
+  template
+  void submit_convolution_kernel<double, 3>(
+    size_type K,
+    size_type N,
+    const double normr,
+    const double norms,
+    const double fac,
+    const TensorView<double, 3+1>& f,
+    TensorView<double, 3+1>& result,
+    const std::array<TensorView<double, 2>, 3>& transr,
+    const std::array<TensorView<double, 2>, 3>& transs,
+    double* tmp,
+    ttg::device::Stream stream);
+
+} // namespace mra
