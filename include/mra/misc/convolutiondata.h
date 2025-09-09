9ce23d120931903dc915bcb31250672de5877da2
nilesh <n.chaturvedi3@gmail.com>
2025-09-09 11:12:12 -0400

    Remove useless check

--- include/mra/misc/convolutiondata.h
+++ include/mra/misc/convolutiondata.h
@@ -79,7 +79,7 @@ namespace mra {
         auto rnlp_view = rnlp.current_view();
         rnlp_view = 0.0;
 
-        if (lx < 0) lx = -lx-1;
+        // if (lx < 0) lx = -lx-1; // translation is defined to be unsigned
         T scaledcoeff  = coeff*std::pow(0.5, 0.5*n);
         T beta = expnt * std::pow(T(0.25), T(n));
         T h = 1.0/std::sqrt(beta);
