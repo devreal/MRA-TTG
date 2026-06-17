1ed873e217efa2e67b6857d1406efb338ea3eae3
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2026-06-17 19:24:00 -0400

    Bump TTG to get a newer MADNESS
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- cmake/modules/FindOrFetchTTG.cmake
+++ cmake/modules/FindOrFetchTTG.cmake
@@ -11,7 +11,7 @@ FetchContent_Declare(
       ttg
       GIT_REPOSITORY      https://github.com/devreal/ttg.git
 	  #GIT_TAG             ttg-for-mra
-      GIT_TAG             master
+      GIT_TAG             8de74cef3ea24f10f9f3fddc8ca782ebd3b1e5bf
   )
 
 #FetchContent_Declare(
