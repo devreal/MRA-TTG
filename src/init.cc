06173157df0cb11bbd9b7c74d6d67a8529cd022c
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2026-07-02 14:50:35 -0400

    Pass argc/argv to madness::initialize
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>

--- src/init.cc
+++ src/init.cc
@@ -23,7 +23,7 @@ namespace mra {
 #endif // TTG_PARSEC_IMPORTED
     madness::initialize(argc, argv, /* nthread = */ 1, /* quiet = */ true);
     madness::World& world = madness::World::get_default();
-    madness::startup(world, 0, nullptr, false);
+    madness::startup(world, argc, argv, false);
 
 #if !defined(MRA_ENABLE_HOST) && defined(MRA_HAVE_SCRATCH_ALLOCATOR)
     // adjust the pinned memory allocator through TTG's environment settings
